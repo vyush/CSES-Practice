a34803ed1ad748c634045e02d941b81a074f8c9a
Vyush Agarwal <vyush11@gmail.com>
2020-06-27 00:28:59 +0530

    Create Sum0fFourValues.cpp

---
+++ Sum0fFourValues.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define ll long long
+#define db double
+#define ld long double
+#define rep(i,n) for(ll i=0;i<n;i++)
+#define rev(i,n) for(ll i=n;i>=0;i--)
+#define rep_a(i,a,n) for(ll i=a;i<n;i++)
+#define pb push_back
+#define sz(v) ((int)(v).size())
+#define all(v) (v).begin(), (v).end()
+#define debug(x) do { std::cerr << #x << ": " << x << std::endl; } while (0);
+#define debugg(x,y) do { std::cerr << #x << ": " << x << "  "<< #y << ": " << y << std::endl; } while (0);
+#define debugggg(i,j,k,ans) std::cerr << #i << ": " << i << "  "<< #j << ": " << j <<#k << ": " << k <<#ans << ": " << ans << std::endl;
+const long long mod = 998244353;
+const long long MOD = 1000000007;
+ 
+typedef vector<ll>              vll;
+typedef vector<string>          vstr;
+typedef vector<char>            vchar;
+typedef vector<pair<ll,ll> >    vpll;
+typedef set<ll>                 sll;
+typedef set<string>             sstr;
+typedef set<pair<ll,ll> >       spll;
+typedef map<ll,ll>              mllll;
+typedef map<string,ll>          mstrll;
+typedef queue<ll>               qll;
+int solve()
+{
+	int n,x;
+	cin>>n>>x;
+	vll a(n);
+	rep(i,n)cin>>a[i];
+	map<ll,pair<ll,ll>> mp;
+	rep(i,n)
+	{
+		rep_a(j,i+1,n)
+		{
+			if(mp.find(x - a[i] - a[j]) !=mp.end())
+			{
+				cout<<i+1<<" "<<j+1<<" "<<mp[(x - a[i] - a[j])].first<<" "<<mp[(x - a[i] - a[j])].second<<endl;
+				return 0;
+			}
+		}
+		rep(j,i)
+		{
+			mp[a[i]+a[j]] = {i+1,j+1};
+		}
+	}
+	cout<<"IMPOSSIBLE"<<endl;
+	return 0;
+}
+int main()
+{
+  fast;
+	/* #ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#else
+	#endif */
+	solve();
+	/* #ifndef ONLINE_JUDGE
+	cout<<"\nTime Elapsed: "<<1.0*clock()/ CLOCKS_PER_SEC <<" Sec\n";
+	#endif */
+	return 0;
+}
